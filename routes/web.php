32d15742620ad2186749a163afae06b24ee61bc4
RobinPMT <phanminhtuan023654@gmail.com>
2022-10-07 15:16:45 +0700

    link đến trang tự thiết kế

--- routes/web.php
+++ routes/web.php
@@ -76,10 +76,11 @@ Route::get('chinh-sach-mua-hang/', [PolicyController::class, 'indexShopping'])->
 Route::get('chinh-sach-van-chuyen/', [PolicyController::class, 'indexTransport'])->name('get.list.transport.policy');
 Route::get('chinh-sach-bao-hanh/', [PolicyController::class, 'indexInsurance'])->name('get.list.insurance.policy');
 Route::get('tu-thiet-ke/', [DesignController::class, 'index'])->name('get.list.design');
-Route::post('tu-thiet-ke/', [DesignController::class, 'store'])->name('post.store.design');
+
 Route::prefix('/')->middleware('CheckLoginUser')->group(function () {
     Route::get('thay-doi-mat-khau', [AuthController::class, 'getChangePassword'])->name('get.change.password.auth');
     Route::post('thay-doi-mat-khau', [AuthController::class, 'postChangePassword'])->name('post.change.password.auth');
     Route::get('thong-tin-ca-nhan', [AuthController::class, 'getProfile'])->name('get.change.profile.auth');
     Route::post('thong-tin-ca-nhan', [AuthController::class, 'postProfile'])->name('post.change.profile.auth');
+    Route::post('tu-thiet-ke/', [DesignController::class, 'store'])->name('post.store.design');
 });
